09339eb6a31668031a54baabfb0b0666697279be
PawelKawa <ramzord@gmail.com>
2023-01-28 21:08:46 +0000

    first master commit, next on branches

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <header>
+            <nav>
+              <NuxtLink to="/">Home</NuxtLink>
+              <NuxtLink to="/about">About</NuxtLink>
+            </nav>
+        </header>
+        <p class="main">about</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam molestias hic placeat eaque ullam nulla enim unde voluptate totam iste!</p>
+    </div>
+  </template>
+
+<style scoped>
+header{
+  background-color: antiquewhite;
+  height: 10vh;
+}
+.main{
+  text-align: center;
+  font-size: 36px;
+  text-transform: uppercase;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <header>
+        <NuxtLogo />
+          <nav>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/about">About</NuxtLink>
+          </nav>
+      </header>
+      <p class="main">home</p>
+  </div>
+</template>
+
+<style scoped>
+header{
+  background-color: antiquewhite;
+  height: 10vh;
+}
+.main{
+  text-align: center;
+  font-size: 36px;
+  text-transform: uppercase;
+}
+
+</style>
