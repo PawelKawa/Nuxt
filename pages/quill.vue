f37f2f95631fea19a85e9e9cf364208d7014b2f3
PawelKawa <ramzord@gmail.com>
2023-02-02 15:11:35 +0000

    added and imported katex with katex.css

--- pages/quill.vue
+++ pages/quill.vue
@@ -1,19 +1,28 @@
 <template>
+  <!DOCTYPE html>
    <div>
+
     <client-only placeholder="loading...">
        <vue-editor :editorToolbar="customToolbar" v-model="content"></vue-editor>
     </client-only>
-       <p>asd</p>
+
+       <span class="katex">
+        <span class="katex-mathml">The KaTeX stylesheet is not loaded!</span>
+        <span class="katex-version rule">KaTeX stylesheet version: </span>
+      </span>
+
    </div>
   </template>
   
   <script>
-    import { VueEditor } from 'vue2-quill-editor'
-  
+    import { VueEditor } from 'vue2-quill-editor';
+    import { katex } from 'katex';
+    import 'katex/dist/katex.min.css';
+
     export default {
   
     components: {
-       VueEditor
+       VueEditor, katex
     },
   
     data() {
@@ -22,9 +31,14 @@
           customToolbar: [
             ['bold', 'italic', 'underline'],
             [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-            [],
-          ]  
+            ['formula'],
+          ],
         }
       }
     }
   </script> 
+
+<style>
+.katex-version {display: none;}
+.katex-version::after {content:"0.10.2 or earlier";}
+</style>
